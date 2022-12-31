973b4033e57efbddb922ad5dea8bd4a01c7a271d
abdul4344 <abdurrahmankadiri@gmail.com>
2022-12-31 22:56:00 +0000

    Saved progress on IfStatement 2022-12-31 22:56:00 +0000

--- IfStatement/src/main/java/IfStatement.java
+++ IfStatement/src/main/java/IfStatement.java
@@ -1,3 +1,4 @@
+import javax.naming.spi.ResolveResult;
 
 public class IfStatement {
     /**
@@ -17,6 +18,11 @@ public class IfStatement {
      * @return x if bool is true. if bool is false, return y.
      */
     public int ifExample(boolean bool, int x, int y){
-        return 0;
+        
+        if (bool==true) {
+            return x;
+        } else  {
+            return y;
+        }
     }
 }
